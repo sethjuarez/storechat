c13e2cacb469943d082b2f8caf4f24820953d0f3
sethjuarez <me@sethjuarez.com>
2023-01-30 14:23:17 -0800

    attempting corrected build command

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
-import App from 'next/app'
+import "../styles/globals.css";
+import App from "next/app";
 import { ThemeProvider } from "@primer/react";
-import { SessionProvider } from "next-auth/react"
-import { withApplicationInsights } from '../services/insights'
+import { SessionProvider } from "next-auth/react";
+import { withApplicationInsights } from "../services/insights";
 
 class ChatApp extends App {
   render() {
@@ -20,5 +20,5 @@ class ChatApp extends App {
 
 export default withApplicationInsights({
   connectionString: process.env.NEXT_PUBLIC_APP_INSIGHTS_CONNECTION || "",
-  isEnabled: true,
+  isEnabled: true //process.env.NODE_ENV === "production",
 })(ChatApp);
