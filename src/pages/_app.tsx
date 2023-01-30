b957afe3f567c770f345f4732943764c704bbb10
sethjuarez <me@sethjuarez.com>
2023-01-30 09:28:14 -0800

    added telemetry

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,24 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import App from 'next/app'
 import { ThemeProvider } from "@primer/react";
 import { SessionProvider } from "next-auth/react"
+import { withApplicationInsights } from '../services/insights'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <SessionProvider session={pageProps.session}>
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </SessionProvider>
-  );
+class ChatApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <SessionProvider session={pageProps.session}>
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SessionProvider>
+    );
+  }
 }
+
+export default withApplicationInsights({
+  instrumentationKey: process.env.APP_INSIGHTS_CONNECTION || "",
+  isEnabled: true,
+})(ChatApp);
